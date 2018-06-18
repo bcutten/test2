d37e5c368054c0af81d7c8d6dbd52c1e699bfc20
bcutten <bcutten@ugcloud.ca>
2018-06-18 13:38:31 -0400

    printing stuff

---
+++ Test2/src/test2/Test2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test2;
+
+/**
+ * This is an application which
+ * Created 18-Jun-2018 on 1:35:49 PM
+ * @author B. Cutten
+ */
+public class Test2 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
